15eedec25c8edb52647bd8f7756017195c6b030e
AndersonFreixo <anderson.freixo@gmail.com>
2023-03-24 12:43:36 -0300

    add (metodo de login do técnico)

--- src/main/java/micromaintainsys/control/MainController.java
+++ src/main/java/micromaintainsys/control/MainController.java
@@ -16,8 +16,24 @@ public class MainController {
     private ArrayList<Ordem> ordensFinalizadas;
     private ArrayList<OrdemCompra> ordensCompras;
     private ArrayList<Fatura> faturas;
+    //Armazena o ID do tecnico logado no sistema
+    private int tecnicoSessaoID;
+
     public void criaTecnico(String nome, String senha){
         Tecnico novoTecnico = new Tecnico(nome, senha, null, null);
         this.tecnicosCadastrados.put(novoTecnico.getTecnicoID(), novoTecnico);
+    }
+
+    public boolean loginTecnico(String id, String senha){
+        Tecnico tecnico = tecnicosCadastrados.get(id);
+        if (tecnico != null){
+            this.tecnicoSessaoID = tecnico.getTecnicoID();
+            return true;
+        }
+        return false;
+    }
+    /*TODO*/
+    public boolean logoutTecnico(){
+        return true;
     }
 }
