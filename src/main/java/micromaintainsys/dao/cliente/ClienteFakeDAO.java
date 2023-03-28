7066d9f09b4570be187df9695fc0aa9874cd7f79
Keven C <keven_jkc@hotmail.com>
2023-03-28 11:17:12 -0300

    feat: documentação Javadoc para classe Cliente

--- src/main/java/micromaintainsys/dao/cliente/ClienteFakeDAO.java
+++ src/main/java/micromaintainsys/dao/cliente/ClienteFakeDAO.java
@@ -6,11 +6,27 @@ import micromaintainsys.model.Ordem;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
+/**
+ * Esta classe implementa um DAO para gerenciar objetos do tipo Cliente
+ * Ela fornece métodos para inserir, atualizar, excluir e buscar clientes.
+ *
+ * @author Keven e Freixo
+ * @version 1.0
+
+*/
 public class ClienteFakeDAO implements ClienteDAO {
     /*Alterar lógica de banco de dados */
     static Hashtable<Integer, Cliente> clientesCadastrados;
     private static int idCounter = 0;
 
+    /**
+     * Cria um objeto do tipo Cliente
+     *
+     * @param endereco
+     * @param nome
+     * @param telefone
+     * @return O ID do Cliente criado
+     */
     public int create(String endereco, String nome, String telefone) {
         Cliente novoCliente = new Cliente(endereco, nome, telefone, new ArrayList<Ordem>());
         novoCliente.setClienteID(idCounter);
@@ -19,9 +35,23 @@ public class ClienteFakeDAO implements ClienteDAO {
         return novoCliente.getId();
     }
 
+    /**
+     * Busca uma pessoa pelo seu ID
+     *
+     * @param clienteId
+     * @return O cliente encontrado, ou null se não existir
+     */
     public Cliente findById(int clienteId) {return clientesCadastrados.get(clienteId);}
 
 
+    /**
+     * Atualiza um atributo de um cliente no mapa de clientes cadastrados.
+     *
+     * @param clienteId o ID do cliente a ser atualizado
+     * @param atributo o nome do atributo a ser atualizado ("nome", "endereco" ou "telefone")
+     * @param valor o novo valor para o atributo
+     * @return true se o cliente foi atualizado com sucesso, ou false se o ID do cliente não foi encontrado ou se o nome do atributo é inválido
+     */
     @Override
     public <T> boolean update(int clienteId, String atributo, T valor) {
         Cliente cliente = clientesCadastrados.get(clienteId);
