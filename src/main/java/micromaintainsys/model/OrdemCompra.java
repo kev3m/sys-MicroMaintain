881ae878d008b9fa94b7688d37d27ced665ff7ce
AndersonFreixo <anderson.freixo@gmail.com>
2023-04-07 15:18:11 -0300

    feat: adciona setters e getters de OrdemCompra

--- src/main/java/micromaintainsys/model/OrdemCompra.java
+++ src/main/java/micromaintainsys/model/OrdemCompra.java
@@ -1,14 +1,48 @@
 package micromaintainsys.model;
 
+import java.util.Calendar;
+
 public class OrdemCompra {
     private String peca;
     private int quantidade;
     private double valorUnitario;
+    private Calendar dataCriacao = Calendar.getInstance();
 
     public OrdemCompra(String peca, int quantidade, double valorUnitario){
         this.peca = peca;
         this.quantidade = quantidade;
         this.valorUnitario = valorUnitario;
+    }
 
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public String getPeca() {
+        return peca;
+    }
+
+    public void setPeca(String peca) {
+        this.peca = peca;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Calendar getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Calendar dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 }
