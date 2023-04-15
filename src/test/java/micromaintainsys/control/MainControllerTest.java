41da52dd0abfe5778955df2db49377d45a8dadc1
AndersonFreixo <anderson.freixo@gmail.com>
2023-04-15 17:37:45 -0300

    add: teste completo do sistema

--- src/test/java/micromaintainsys/control/MainControllerTest.java
+++ src/test/java/micromaintainsys/control/MainControllerTest.java
@@ -2,13 +2,20 @@ package micromaintainsys.control;
 
 import junit.framework.TestCase;
 import micromaintainsys.dao.DAO;
-import micromaintainsys.model.Tecnico;
+import micromaintainsys.exceptions.InvalidUserException;
+import micromaintainsys.exceptions.UserAlreadyLoggedInException;
+import micromaintainsys.exceptions.WrongPasswordException;
+import micromaintainsys.model.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Queue;
 
 public class MainControllerTest extends TestCase {
     private MainController controller = new MainController();
@@ -24,15 +31,85 @@ public class MainControllerTest extends TestCase {
     @Test
     public void testCriaELogaTecnico(){
         controller.loginTecnico(0, "admin");
-        Tecnico tecnico = controller.criaTecnico("Joao", "123");
-        assertNotNull(tecnico);
-        assertEquals("Joao", tecnico.getNome());
+        Tecnico tecnicoCriado = controller.criaTecnico("Joao", "123");
+        assertNotNull(tecnicoCriado);
+        assertEquals("Joao", tecnicoCriado.getNome());
         List<Tecnico> cadastrados = controller.listaTecnicos();
         assertEquals(2, cadastrados.size());
         controller.logoutTecnico();
-        controller.loginTecnico(tecnico.getTecnicoID(), "123");
-        assertEquals(controller.getTecnicoSessao().getNome(), tecnico.getNome());
+        controller.loginTecnico(tecnicoCriado.getTecnicoID(), "123");
+        assertEquals(controller.getTecnicoSessao().getNome(), tecnicoCriado.getNome());
+        controller.logoutTecnico();
+    }
+    @Test
+    public void testSenhaErrada(){
+        boolean wrongPass = false;
+        try{
+            controller.loginTecnico(0, "admino");
+        }
+        catch (WrongPasswordException e){
+            wrongPass = true;
+        }
+        assertEquals(true, wrongPass);
+    }
+    @Test
+    public void testLoginUsuarioLogado(){
+        boolean alreadyLogged = false;
+        controller.loginTecnico(0, "admin");
+        try{
+            controller.loginTecnico(0, "admin");
+        }
+        catch (UserAlreadyLoggedInException e){
+            alreadyLogged = true;
+        }
+        assertEquals(true, alreadyLogged);
+    }
+    @Test
+    public void testLoginUsuarioInvalido(){
+        boolean invalidUser = false;
+        try{
+            controller.loginTecnico(999, "admin");
+        }
+        catch (InvalidUserException e){
+            invalidUser = true;
+        }
+        assertEquals(true, invalidUser);
+    }
+    public void testSistemaCompleto(){
+        Calendar inicioProcesso = Calendar.getInstance();
+        /*Cria técnico*/
+        controller.loginTecnico(0, "admin");
+        Tecnico novoTecnico = controller.criaTecnico("Pedro", "senha");
+        Queue<Ordem> abertas = controller.getOrdensAbertas();
         controller.logoutTecnico();
 
+        /*Loga no novo técnico e cadastra cliente*/
+                controller.loginTecnico(novoTecnico.getTecnicoID(), "senha");
+        Cliente clienteCriado = controller.criaCliente("Geraldo", "Rua Nascimento Silva 107", "7199999999");
+
+        /*Compra peça, cadastra ordem e serviços e atribui ao técnico*/
+        controller.compraPeca("bateriaBIOS", 50, 0.5);
+        Ordem ordemCriada = controller.criaOrdem(clienteCriado.getId());
+        Servico servico1 = controller.criaServico(CategoriaServico.Limpeza, 75.5, "", "Limpeza completa", ordemCriada.getOrdemID());
+        Servico servico2 = controller.criaServico(CategoriaServico.Montagem, 30, "bateriaBIOS", "Trocar bateria da BIOS", ordemCriada.getOrdemID());
+
+        controller.atribuiOrdem(novoTecnico.getTecnicoID());
+
+        /*Encerra serviços e ordem e faz pagamento*/
+        controller.encerraServico(servico1.getServicoID());
+        controller.encerraServico(servico2.getServicoID());
+        controller.fechaOrdem(ordemCriada.getOrdemID());
+        Fatura fatura = controller.geraFatura(ordemCriada.getOrdemID());
+        controller.realizaPagamento(TipoPagamento.Dinheiro, 50, fatura.getFaturaID());
+        controller.realizaPagamento(TipoPagamento.Pix, 55.5, fatura.getFaturaID());
+        assertEquals(StatusOrdem.Finalizada, ordemCriada.getStatus());
+        Calendar fimProcesso = Calendar.getInstance();
+        RelatorioCompras relatorioCompras = controller.geraRelatorioCompras(inicioProcesso, fimProcesso);
+        assertEquals(25.0, relatorioCompras.getValorTotal());
+        assertEquals(1, relatorioCompras.getOrdens().size());
+        RelatorioServicos relatorioServicos = controller.geraRelatorioServicos(inicioProcesso, fimProcesso);
+        assertEquals(2, relatorioServicos.getTotalEncerrados());
+        assertEquals(0, relatorioServicos.getTotalEmAberto());
     }
+
 }
