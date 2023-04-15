6d0a4c865f6c9b92ec22bfc53515e2b7df9352c1
AndersonFreixo <anderson.freixo@gmail.com>
2023-04-15 13:06:35 -0300

    add: primeiros testes do sistema

--- src/test/java/micromaintainsys/control/MainControllerTest.java
+++ src/test/java/micromaintainsys/control/MainControllerTest.java
@@ -1,14 +1,38 @@
 package micromaintainsys.control;
 
 import junit.framework.TestCase;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.Tecnico;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainControllerTest extends TestCase {
     private MainController controller = new MainController();
+
+    @Test
     public void testLoginLogoutAdm() {
         controller.loginTecnico(0, "admin");
         assertNotNull(controller.getTecnicoSessao());
-        assertEquals(controller.getTecnicoSessao().isAdm(), true);
+        assertEquals(true, controller.getTecnicoSessao().isAdm());
         controller.logoutTecnico();
-        assertNotNull(controller.getTecnicoSessao());
+        assertNull(controller.getTecnicoSessao());
+    }
+    @Test
+    public void testCriaELogaTecnico(){
+        controller.loginTecnico(0, "admin");
+        Tecnico tecnico = controller.criaTecnico("Joao", "123");
+        assertNotNull(tecnico);
+        assertEquals("Joao", tecnico.getNome());
+        List<Tecnico> cadastrados = controller.listaTecnicos();
+        assertEquals(2, cadastrados.size());
+        controller.logoutTecnico();
+        controller.loginTecnico(tecnico.getTecnicoID(), "123");
+        assertEquals(controller.getTecnicoSessao().getNome(), tecnico.getNome());
+        controller.logoutTecnico();
+
     }
 }
