8f9975489bbf5e16bab57a1616d86b245b12149f
Keven C <keven_jkc@hotmail.com>
2023-04-30 21:44:50 -0300

    test: alteração no EstoqueDAOTest

--- src/test/java/sysmicromaintain/dao/EstoqueDAOTest.java
+++ src/test/java/sysmicromaintain/dao/EstoqueDAOTest.java
@@ -2,25 +2,22 @@ package sysmicromaintain.dao;
 
 import micromaintainsys.dao.estoque.EstoqueDAO;
 import micromaintainsys.model.Estoque;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class EstoqueDAOTest {
     private EstoqueDAO estoqueDAO;
-
+    private Estoque estoque;
     @Before
-    public void setUp() {
+  public void setUp(){
         estoqueDAO = new EstoqueDAO();
     }
 
     @Test
-    public void testCriaEstoque() {
-        Estoque estoque = estoqueDAO.carrega();
-
+    public void testCarregaEstoque(){
+        estoque = estoqueDAO.carrega();
         Assert.assertNotNull(estoque);
-        Assert.assertNotNull(estoque.getPecas());
-        Assert.assertNotNull(estoque.verificaOrdensCompra());
     }
-
 }
