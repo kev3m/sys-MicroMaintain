9f6b8c5e508e9e074eeabb517fd42e3a771cdc27
Keven C <keven_jkc@hotmail.com>
2023-05-26 17:34:18 -0300

    feat: tabela de técnicos funcional

--- src/main/java/micromaintainsys/control/tecnicosController.java
+++ src/main/java/micromaintainsys/control/tecnicosController.java
@@ -1,22 +1,47 @@
 package micromaintainsys.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import micromaintainsys.model.SceneSwitch;
+import javafx.scene.control.TableView;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
-public class tecnicosController {
+public class tecnicosController implements Initializable {
 
     @FXML
     private AnchorPane tecnicosAnchorPane;
+    private Queue<Ordem> ordensAbertas;
+    private ArrayList<Ordem> ordensServico;
+    private ArrayList<Fatura> faturas;
+    private Estoque estoque;
 
     @FXML
-    void closeProgram(MouseEvent event) {
+    private TableColumn<Tecnico, Boolean> PermColumn;
+    @FXML
+    private TableColumn<Tecnico, Integer> idColumn;
 
-    }
+    @FXML
+    private TableColumn<Tecnico, String> nomeColumn;
+
+    @FXML
+    private TableColumn<Tecnico, Integer> ordemAColumn;
+
+    @FXML
+    private TableColumn<Tecnico, String> senhaColumn;
+
+    @FXML
+    private TableView<Tecnico> tableView;
     @FXML
     void switchToOrdem() throws IOException {
         new SceneSwitch(tecnicosAnchorPane, "main.fxml");
@@ -42,5 +67,17 @@ public class tecnicosController {
         new SceneSwitch(tecnicosAnchorPane, "ordem_compra.fxml");
     }
 
+    ObservableList<Tecnico> observableList = FXCollections.observableArrayList(DAO.getTecnicoDAO().pegaTodos());
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.idColumn.setCellValueFactory(new PropertyValueFactory<Tecnico, Integer>("tecnicoID"));
+        this.nomeColumn.setCellValueFactory(new PropertyValueFactory<Tecnico, String>("nome"));
+        this.ordemAColumn.setCellValueFactory(new PropertyValueFactory<Tecnico, Integer>("ordemEmAndamentoID"));
+        this.senhaColumn.setCellValueFactory(new PropertyValueFactory<Tecnico, String>("senha"));
+        this.PermColumn.setCellValueFactory(new PropertyValueFactory<Tecnico, Boolean>("permissao"));
+        this.tableView.getItems().setAll(DAO.getTecnicoDAO().pegaTodos());
+        tableView.setItems(observableList);
 
+
+    }
 }
