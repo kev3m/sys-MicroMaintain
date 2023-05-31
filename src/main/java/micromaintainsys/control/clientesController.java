c5483ae88df4e3bfe458581bed15965ccd07432d
Keven C <keven_jkc@hotmail.com>
2023-05-31 15:31:28 -0300

    feat: Implementação CRUD funcional na tela de gerenciamento de Clientes

--- src/main/java/micromaintainsys/control/clientesController.java
+++ src/main/java/micromaintainsys/control/clientesController.java
@@ -1,16 +1,43 @@
 package micromaintainsys.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.Cliente;
+import micromaintainsys.model.Ordem;
 import micromaintainsys.model.SceneSwitch;
+import micromaintainsys.model.Tecnico;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class clientesController {
+public class clientesController implements Initializable {
 
     @FXML
     private AnchorPane clientesAnchorPane;
+    @FXML
+    private TableView<Cliente> tableView;
+    @FXML
+    private TableColumn<Cliente, Integer> clientID;
+    @FXML
+    private TableColumn<Cliente, String> clientNome;
+    @FXML
+    private TableColumn<Cliente, String> clientEndereco;
+    @FXML
+    private TableColumn<Cliente, String> clientTel;
+    @FXML
+    private TableColumn<Cliente, ArrayList<Ordem>> clientOrdens;
+
+
 
     @FXML
     private ImageView closeButton;
@@ -18,6 +45,10 @@ public class clientesController {
     @FXML
     void switchToOrdem() throws IOException {
         new SceneSwitch(clientesAnchorPane, "main.fxml");
+    }
+    @FXML
+    void switchToOrdem_ger() throws IOException {
+        new SceneSwitch(clientesAnchorPane, "management_Scenes/clientes_ger.fxml");
     }
     @FXML
     void switchToTec() throws IOException {
@@ -40,5 +71,19 @@ public class clientesController {
         new SceneSwitch(clientesAnchorPane, "ordem_compra.fxml");
     }
 
+    ObservableList<Cliente> observableList = FXCollections.observableArrayList(DAO.getClienteDAO().pegaTodos());
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.clientID.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        this.clientNome.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        this.clientEndereco.setCellValueFactory(new PropertyValueFactory<>("endereco"));
+        this.clientTel.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+        this.clientOrdens.setCellValueFactory(new PropertyValueFactory<>("ordens"));
 
+
+        this.tableView.getItems().setAll(DAO.getClienteDAO().pegaTodos());
+        tableView.setItems(observableList);
+
+
+    }
 }
