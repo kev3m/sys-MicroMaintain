4c296eddb202d9c9cedadf0875f129a711e2fd47
Keven C <keven_jkc@hotmail.com>
2023-06-02 16:39:38 -0300

    add: tela CRUD das Ordens

---
+++ src/main/java/micromaintainsys/control/management_Controllers/ordens_GerController.java
@@ -0,0 +1,202 @@
+package micromaintainsys.control.management_Controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.ResourceBundle;
+
+import static micromaintainsys.utils.ViewUtils.*;
+
+
+public class ordens_GerController implements Initializable {
+
+    @FXML
+    private AnchorPane tecnicosAnchorPane;
+    private Queue<Ordem> ordensAbertas;
+    private ArrayList<Ordem> ordensServico;
+    private ArrayList<Fatura> faturas;
+    private Estoque estoque;
+
+    @FXML
+    private Button UpdateButton;
+
+    @FXML
+    private Button UpdateButton1;
+
+    @FXML
+    private Button addTecButton;
+
+    @FXML
+    private ImageView closeButton;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField idRemoveField1;
+
+    @FXML
+    private TextField tecField;
+
+    @FXML
+    private TextField nameFieldUpdate;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private Button servicosButton;
+
+    @FXML
+    private Button servicosPageButtonUp;
+
+    @FXML
+    private Button tecButton;
+    @FXML
+    private ChoiceBox<?> statusChoice;
+
+    @FXML
+    void addOrdem() throws IOException {
+        if (idField.getText().isEmpty()){
+            showWarningAlert("Campo vazio", "Para prosseguir, preencha todos os campos");
+        }
+        else if (DAO.getClienteDAO().pegaPorId(Integer.parseInt(idField.getText())) == null){
+            showErrorAlert("Cliente não encontrado", "Por favor, insira um ID válido");
+        }
+        else{
+            int id = Integer.parseInt(idField.getText());
+            Cliente cliente = DAO.getClienteDAO().pegaPorId(id);
+            Ordem ordem = new Ordem(Cliente);
+            DAO.getOrdemDAO().cria(ordem.getClienteID());
+            new SceneSwitch(tecnicosAnchorPane, "tecnicos.fxml");
+
+        }
+
+    }
+
+    @FXML
+    void searchTec(){
+        String id = idField.getText();
+        if (id.isEmpty() || DAO.getTecnicoDAO().pegaPorId(Integer.parseInt(id)) == null){
+            showErrorAlert("Campo vazio ou ID inválido", "Por favor, verifique o campo de ID");
+        }
+        else{
+            fillUpdateFields();
+        }
+    }
+
+
+    void fillUpdateFields(){
+        int id = Integer.parseInt(idField.getText());
+        String name = DAO.getTecnicoDAO().pegaPorId(id).getNome();
+        String pass = DAO.getTecnicoDAO().pegaPorId(id).getSenha();
+        int order = DAO.getTecnicoDAO().pegaPorId(id).getOrdemEmAndamentoID();
+        boolean adm = DAO.getTecnicoDAO().pegaPorId(id).isAdm();
+        nameFieldUpdate.setText(name);
+        passFieldUpdate.setText(pass);
+        OrderFieldUpdate.setText(String.valueOf(order));
+        admCheck.setSelected(adm);
+    }
+
+    @FXML
+    void clearUpdateFields(){
+        nameFieldUpdate.setText("");
+        passFieldUpdate.setText("");
+        OrderFieldUpdate.setText("");
+        admCheck.setSelected(false);
+
+    }
+    @FXML
+    void updateTec(){
+        String idText = idField.getText();
+        String name = nameFieldUpdate.getText();
+        String pass = passFieldUpdate.getText();
+        String order = OrderFieldUpdate.getText();
+        boolean adm = admCheck.isSelected();
+        if (idText.isEmpty() || DAO.getTecnicoDAO().pegaPorId(Integer.parseInt(idText)) == null){
+            showErrorAlert("Campo vazio ou ID inválido", "Por favor, verifique o campo de ID");
+        }
+        else{
+            if (nameFieldUpdate.getText().isEmpty() || passFieldUpdate.getText().isEmpty() || OrderFieldUpdate.getText().isEmpty()){
+                showWarningAlert("Campo vazio", "Para prosseguir, preencha todos os campos");
+            }
+            else {
+                Tecnico tecnico = DAO.getTecnicoDAO().pegaPorId(Integer.parseInt(idText));
+                tecnico.setNome(name);
+                tecnico.setSenha(pass);
+                tecnico.setOrdemEmAndamentoID(Integer.parseInt(order));
+                tecnico.setAdm(adm);
+                DAO.getTecnicoDAO().atualiza(tecnico);
+                showInformationAlert("Técnico atualizado", "O técnico foi atualizado com sucesso");
+            }
+        }
+    }
+    @FXML
+    void removeTec(){
+        String idText = idRemoveField.getText();
+        if (idText.isEmpty() || DAO.getTecnicoDAO().pegaPorId(Integer.parseInt(idText)) == null){
+            showErrorAlert("Campo vazio ou ID inválido", "Por favor, verifique o campo de ID");
+
+        }
+        else{
+            DAO.getTecnicoDAO().remove(Integer.parseInt(idText));
+            showInformationAlert("Técnico removido", "O técnico foi removido com sucesso");
+        }
+
+    }
+
+    @FXML
+    void switchToOrdem() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "main.fxml");
+    }
+    @FXML
+    void switchToTec() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "tecnicos.fxml");
+    }
+    @FXML
+    void switchToClientes() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "clientes.fxml");
+    }
+    @FXML
+    void switchToEstoque() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "estoque.fxml");
+    }
+    @FXML
+    void switchToFatura() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "faturas.fxml");
+    }
+    @FXML
+    void switchToOrdem_Compra() throws IOException {
+        new SceneSwitch(tecnicosAnchorPane, "ordem_compra.fxml");
+    }
+
+    ObservableList<Tecnico> observableList = FXCollections.observableArrayList(DAO.getTecnicoDAO().pegaTodos());
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Configurar o filtro de entrada para aceitar somente números
+            numberFilter(idField);
+            numberFilter(idRemoveField);
+//            numberFilter(OrderFieldUpdate);
+
+        // Configura o filtro de entrada para aceitar letras
+            letterFilter(nameField);
+            letterFilter(nameFieldUpdate);
+
+
+
+    }
+}
--- src/main/java/micromaintainsys/control/ordersController.java
+++ src/main/java/micromaintainsys/control/ordersController.java
@@ -1,23 +1,91 @@
 package micromaintainsys.control;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import micromaintainsys.model.SceneSwitch;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.*;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
-public class ordersController {
-
+public class ordersController implements Initializable {
     @FXML
-    private ImageView closeButton;
+    private TableView<Ordem> tableView;
+//
+//    @FXML
+//    private TableColumn<Ordem, int> idColumn;
+//    @FXML
+//    private TableColumn<Servico, Calendar> dataColumn;
+//    @FXML
+//    private TableColumn<Ordem, int> clienteColumn;
+//    @FXML
+//    private TableColumn<Ordem, int> tecnicoColumn;
+//    @FXML
+//    private TableColumn<Ordem, StatusOrdem> statusColumn;
+//    @FXML
+//    private TableColumn<Pagamento, TipoPagamento> pagamentoColumn;
+//
+//    @FXML
+//    private TableColumn<Fatura, double> valorColumn;
 
     @FXML
     private AnchorPane ordersAnchorPane;
+    private Queue<Ordem> ordensAbertas;
+    private ArrayList<Ordem> ordensServico;
+    private ArrayList<Fatura> faturas;
+    private Estoque estoque;
+
+    ObservableList<Ordem> observableList = FXCollections.observableArrayList(ordensAbertas
+    );
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        this.estoque = carregaEstoque();
+        ArrayList<Ordem> abertas = DAO.getOrdemDAO().pegaTodasPorStatus(StatusOrdem.Aberta);
+        Collections.sort(abertas);
+        this.ordensAbertas = new LinkedList<>(abertas);
+        this.ordensServico = new ArrayList<>(DAO.getOrdemDAO().pegaTodas());
+        this.faturas = new ArrayList<>(DAO.getFaturaDAO().pegaTodas());
+
+//        this.idColumn.setCellValueFactory(new PropertyValueFactory<Ordem, int>("ordemID"));
+//        this.dataColumn.setCellValueFactory(new PropertyValueFactory<Servico, Calendar>("horarioAbertura"));
+//        this.clienteColumn.setCellValueFactory(new PropertyValueFactory<Ordem, int>("clienteID"));
+//        this.tecnicoColumn.setCellValueFactory(new PropertyValueFactory<Ordem, int>("tecnicoID"));
+//        this.statusColumn.setCellValueFactory(new PropertyValueFactory<Ordem, StatusOrdem>("status"));
+//        this.pagamentoColumn.setCellValueFactory(new PropertyValueFactory<Pagamento, TipoPagamento>("tipoPagamento"));
+//        this.valorColumn.setCellValueFactory(new PropertyValueFactory<Fatura, double>("valorTotal"));
+
+        tableView.setItems(observableList);
+
+//        TableColumn orderId = new TableColumn("ID");
+//        TableColumn orderData = new TableColumn("Data");
+//        TableColumn orderCliente = new TableColumn("Cliente");
+//        TableColumn orderTecnico = new TableColumn("Tecnico");
+//        TableColumn orderStatus = new TableColumn("Status");
+//        TableColumn orderPagamento = new TableColumn("Pagamento");
+//        TableColumn orderValor = new TableColumn("Valor");
+    }
+    public Estoque carregaEstoque(){
+        //this.estoque = DAO.getEstoqueDAO().cria()
+        return DAO.getEstoqueDAO().carrega();
+    }
 
     @FXML
     void switchToOrdem() throws IOException {
         new SceneSwitch(ordersAnchorPane, "main.fxml");
+    }
+    @FXML
+    void switchToOrdem_ger() throws IOException {
+        new SceneSwitch(ordersAnchorPane, "management_Scenes/ordens_ger.fxml");
     }
     @FXML
     void switchToTec() throws IOException {
@@ -39,6 +107,17 @@ public class ordersController {
     void switchToOrdem_Compra() throws IOException {
         new SceneSwitch(ordersAnchorPane, "ordem_compra.fxml");
     }
+
+//    @FXML
+//    public boolean logoutTecnico(){
+//        if (this.tecnicoSessao != null){
+//            this.tecnicoSessao = null;
+//            return true;
+//        }
+//        else{
+//            return false;
+//        }
+//    }
 
 
 
