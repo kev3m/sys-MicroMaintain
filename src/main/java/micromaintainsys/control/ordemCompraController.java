1cf959fd2fea3294380a10150ef7b533caa89e9b
Keven C <keven_jkc@hotmail.com>
2023-06-05 23:21:45 -0300

    feat: tabela de Ordens de Compra funcional

--- src/main/java/micromaintainsys/control/ordemCompraController.java
+++ src/main/java/micromaintainsys/control/ordemCompraController.java
@@ -1,20 +1,45 @@
 package micromaintainsys.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import micromaintainsys.dao.DAO;
+import micromaintainsys.model.OrdemCompra;
 import micromaintainsys.model.SceneSwitch;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.ResourceBundle;
 
-public class ordemCompraController {
+public class ordemCompraController implements Initializable {
     @FXML
     private AnchorPane ordemCompraAnchorPane;
+    @FXML
+    private TableColumn<OrdemCompra, Calendar> dataColumn;
+
+    @FXML
+    private TableColumn<OrdemCompra, String> pecaColumn;
+
+    @FXML
+    private TableColumn<OrdemCompra, Integer> quantColumn;
+
+    @FXML
+    private TableColumn<OrdemCompra, Double> valorColumn;
+    @FXML
+    private TableView<OrdemCompra> tableView;
 
     @FXML
     void switchToOrdem() throws IOException {
         new SceneSwitch(ordemCompraAnchorPane, "main.fxml");
     }
+
     @FXML
     void switchToTec() throws IOException {
         new SceneSwitch(ordemCompraAnchorPane, "tecnicos.fxml");
@@ -28,6 +53,10 @@ public class ordemCompraController {
         new SceneSwitch(ordemCompraAnchorPane, "estoque.fxml");
     }
     @FXML
+    void switchToGerEstoque() throws IOException {
+        new SceneSwitch(ordemCompraAnchorPane, "management_Scenes/estoque_ger.fxml");
+    }
+    @FXML
     void switchToFatura() throws IOException {
         new SceneSwitch(ordemCompraAnchorPane, "faturas.fxml");
     }
@@ -36,4 +65,18 @@ public class ordemCompraController {
         new SceneSwitch(ordemCompraAnchorPane, "ordem_compra.fxml");
     }
 
+    ObservableList<OrdemCompra> observableList = FXCollections.observableArrayList(DAO.getEstoqueDAO().carrega().verificaOrdensCompra());
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.dataColumn.setCellValueFactory(new PropertyValueFactory<>("dataCriacao"));
+        this.pecaColumn.setCellValueFactory(new PropertyValueFactory<>("peca"));
+        this.quantColumn.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+        this.valorColumn.setCellValueFactory(new PropertyValueFactory<>("valorUnitario"));
+
+
+
+        this.tableView.getItems().setAll(DAO.getEstoqueDAO().carrega().verificaOrdensCompra());
+        tableView.setItems(observableList);
+
+    }
 }
