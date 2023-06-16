168e4b0596933ce2ee220eb24c911a4e240780a5
AndersonFreixo <anderson.freixo@gmail.com>
2023-06-16 16:57:34 -0300

    feat: cria usuário na primeira execução do programa

--- src/main/java/micromaintainsys/Main.java
+++ src/main/java/micromaintainsys/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import micromaintainsys.dao.DAO;
 
 
 import java.io.File;
@@ -29,6 +30,8 @@ public class Main extends Application{
 
     }
     public static void main(String[] args){
+        /*Apenas pra criar o admin, caso seja a primeira execução*/
+        DAO.getTecnicoDAO().pegaTodos();
         launch(args);
     }
 
